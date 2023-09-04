f29d0f6f5c120d15270209904b9a336e21de55f3
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-09-04 13:47:14 -0300

    utilizando rerender

--- src/componentes/Extrato/Transacoes/Transacoes.test.js
+++ src/componentes/Extrato/Transacoes/Transacoes.test.js
@@ -1,11 +1,29 @@
-import userEvent from '@testing-library/user-event';
-const { render, screen } = require('@testing-library/react');
+import { render, screen } from '@testing-library/react';
 import Transacoes from './index';
-
-
+import estilos from '../Extrato.module.css';
 
 test('Deve renderizar o mesmo component com props atualizadas', () => {
-  render(<Transacoes transacoes={transacao} />);
-  const lista = screen.getByRole('listitem');
-  expect(lista).toBeInTheDocument();
+  const transacao = {
+    transacao: 'Depósito',
+    valor: 100,
+  };
+  const { rerender } = render(
+    <Transacoes estilos={estilos} transacoes={transacao} />,
+  );
+  const tipoTransacao = screen.getByTestId('tipoTransacao');
+  const valorTransacao = screen.getByTestId('valorTransacao');
+
+  expect(tipoTransacao).toHaveTextContent('Depósito');
+  expect(valorTransacao).toHaveTextContent('R$ 100');
+
+  const novaTransacao = {
+    transacao: 'Transferência',
+    valor: 50,
+  };
+  rerender(<Transacoes estilos={estilos} transacoes={novaTransacao} />);
+  const novoTipoTransacao = screen.getByTestId('tipoTransacao');
+  const novoValorTransacao = screen.getByTestId('ValorTransacao');
+
+  expect(novoTipoTransacao).toHaveTextContent('Tranferência');
+  expect(novoValorTransacao).toHaveTextContent('- R$ 50');
 });
