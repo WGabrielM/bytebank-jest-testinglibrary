2d164a54eb0a995838e32642996e04d2a94762eb
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-09-11 10:38:56 -0300

    adicionando teste no Utils

---
+++ src/utils/Utils.test.js
@@ -0,0 +1,30 @@
+import { calculaNovoSaldo } from './index';
+
+test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+  // fn() permite utilizar funções dentro dos testes
+  const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
+
+  const saldo = 100;
+
+  const novoSaldo = calculaRendimento(saldo);
+
+  expect(novoSaldo).toBe(100.5);
+  // Verifica se foi chamado essa função
+  expect(calculaRendimento).toBeCalled();
+  // Verifica se foi chamado com os argumentos específicos
+  expect(calculaRendimento).toHaveBeenCalledWith(saldo);
+});
+
+// Conceito do TDD, criar um teste para falhar e realizar os devidos ajustes
+describe('Quando eu realizo uma transação', () => {
+  test('Que é um depósito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(150);
+  });
+});
